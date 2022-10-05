9f8ab0aa0dfbf2afcf07ba87570bc7b458634534
Lucas25 <lucas@it-up.com.ar>
2022-10-05 20:20:18 -0300

    first commit

---
+++ .history/assets/index_20220911205213.js
@@ -0,0 +1,70 @@
+let pizza1 = {
+    id: '1',
+    nombre: 'Muzarella',
+    ingredientes: [
+        'tomate', 'muzarella', 'oregano'
+    ],
+    precio: '600'
+}
+let pizza2 = {
+    id: '2',
+    nombre: 'Napolitana',
+    ingredientes: [
+        'jamon', 'huevo', 'queso'
+    ],
+    precio: '300'
+}
+let pizza3 = {
+    id: '3',
+    nombre: 'Americana',
+    ingredientes: [
+        'huevoFrito', 'peperoni', 'roquefort'
+    ],
+    precio: '200'
+}
+let pizza4 = {
+    id: '4',
+    nombre: 'Rucula',
+    ingredientes: [
+        'verduras', 'palmito', 'especias'
+    ],
+    precio: '500'
+}
+let pizza5 = {
+    id: '5',
+    nombre: 'Especial',
+    ingredientes: [
+        'salsa', 'harina', 'crema'
+    ],
+    precio: '800'
+}
+let pizza6 = {
+    id: '6',
+    nombre: 'DE_LA_CASA',
+    ingredientes: [
+        'anchoas', 'pimientos', 'hongos'
+    ],
+    precio: '900'
+}
+
+let pizzas = [pizza1, pizza2, pizza3, pizza4, pizza5, pizza6]
+let listado = document.getElementById('listado-pizzas');
+
+const buscarPizza = () => {
+    let valueid = document.getElementById('value-id').value;
+    var precioPizza = document.getElementById('precio-pizza');
+    var inputVacio = document.getElementById('input-vacio')
+    inputVacio.textContent = "";
+
+    validarInputBlanco(valueid, inputVacio)
+
+    var found = pizzas.find(function(post, index) {
+        if (post.id != valueid) return inputVacio.textContent = "El id ingresado no existe.";
+
+        precioPizza.textContent = post.precio;
+    });
+}
+
+const validarInputBlanco = (valueid, inputVacio, post) => {
+    if (valueid == "") return inputVacio.textContent = "Debes ingresar un valor numerico"
+}
---
+++ .history/assets/index_20220927232810.js
@@ -0,0 +1,111 @@
+window.onload = (event) => {
+    var precioPizza = document.getElementById('precio-pizza');
+    var nombrePizza = document.getElementById('nombre-pizza');
+    var ingredientesPizza = document.getElementById('ingredientes-pizza');
+    var imgPizza = document.getElementById("img-pizza");
+    console.log(localStorage.getItem);
+
+    if (localStorage.length > 0) {
+        precioPizza.textContent = localStorage.getItem("precioPizza");
+
+        nombrePizza.textContent = localStorage.getItem("nombrePizza");
+
+        ingredientesPizza.textContent = localStorage.getItem("ingredientesPizza");
+
+        imgPizza.setAttribute('src', localStorage.getItem("imagenPizza"));
+    } else {
+        imgPizza.setAttribute('src', '/img/descarga (4).jpg');
+    }
+};
+let pizza1 = {
+    id: '1',
+    nombre: 'Muzarella',
+    ingredientes: [
+        'tomate', 'muzarella', 'oregano'
+    ],
+    precio: '600',
+    imagen: '/img/descarga (1).jpg',
+}
+let pizza2 = {
+    id: '2',
+    nombre: 'Napolitana',
+    ingredientes: [
+        'jamon', 'huevo', 'queso'
+    ],
+    precio: '300',
+    imagen: '/img/descarga (2).jpg',
+}
+let pizza3 = {
+    id: '3',
+    nombre: 'Americana',
+    ingredientes: [
+        'huevoFrito', 'peperoni', 'roquefort'
+    ],
+    precio: '200',
+    imagen: '/img/descarga (3).jpg',
+}
+let pizza4 = {
+    id: '4',
+    nombre: 'Rucula',
+    ingredientes: [
+        'verduras', 'palmito', 'especias'
+    ],
+    precio: '500',
+    imagen: '/img/descarga.jpg',
+}
+let pizza5 = {
+    id: '5',
+    nombre: 'Especial',
+    ingredientes: [
+        'salsa', 'harina', 'crema'
+    ],
+    precio: '800',
+    imagen: '/img/images (1).jpg',
+}
+let pizza6 = {
+    id: '6',
+    nombre: 'DE_LA_CASA',
+    ingredientes: [
+        'anchoas', 'pimientos', 'hongos'
+    ],
+    precio: '900',
+    imagen: '/img/images.jpg',
+}
+
+let pizzas = [pizza1, pizza2, pizza3, pizza4, pizza5, pizza6]
+let listado = document.getElementById('listado-pizzas');
+
+const buscarPizza = () => {
+    let valueid = document.getElementById('value-id').value;
+    var precioPizza = document.getElementById('precio-pizza');
+    var nombrePizza = document.getElementById('nombre-pizza');
+    var ingredientesPizza = document.getElementById('ingredientes-pizza');
+    var inputVacio = document.getElementById('input-vacio');
+    var imgPizza = document.getElementById("img-pizza");
+    inputVacio.textContent = "";
+    precioPizza.textContent = "";
+    nombrePizza.textContent = "";
+    ingredientesPizza.textContent = "";
+    imgPizza.setAttribute('src', '/img/descarga (4).jpg');
+
+    if (valueid == "") return inputVacio.textContent = "Debes ingresar un valor numerico."
+
+    var found = pizzas.find(function(post, index) {
+        inputVacio.textContent = ""
+        if (post.id == valueid) {
+            precioPizza.textContent = post.precio;
+            localStorage.setItem("precioPizza", post.precio);
+            nombrePizza.textContent = post.nombre;
+            localStorage.setItem("nombrePizza", post.nombre);
+            ingredientesPizza.textContent = post.ingredientes.join(", ");
+            localStorage.setItem("ingredientesPizza", post.ingredientes.join(", "));
+            imgPizza.setAttribute('src', post.imagen);
+            localStorage.setItem("imagenPizza", post.imagen);
+            return true;
+        }
+    });
+
+    if (found == undefined) return inputVacio.textContent = "El codigo ingresado es incorrecto."
+
+
+}
